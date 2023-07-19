b5975c7aee5964a33337300a09fedc27a8a9b760
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-19 05:32:30 +0300

    user oop

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-""" 
-User class
+"""
+This module contains a class called User
 """
 
-class User():
-    """ Documentation """
+
+class User:
+    """ User class """
 
     def __init__(self):
-        """ Documentation """
+        """ Init method """
+
         self.__email = None
+
+    @property
+    def email(self):
+        """ This is a getter for email  """
+
+        return self.__email
 
     @email.setter
     def email(self, value):
-        """ Documentation """
+        """ This is a setter for email  """
+
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
-    
+
 if __name__ == "__main__":
 
     u = User()
